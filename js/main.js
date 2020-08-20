306869f8aefc877f4ac3740351dbd37ccee375d9
Nik <Pengu1n4ik.pi@yandex.ru>
2020-08-20 15:15:22 +0300

    mob-first-site-v.1.0

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function(){
+   $('.top-slider').slick({
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev-arrow.svg" alt="prev"></button>',
+      nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/next-arrow.svg" alt="next"></button>',
+      responsive: [
+         {
+            breakpoint: 1024,
+            settings: {
+               
+            }
+         },
+         {
+            breakpoint: 640,
+            settings: {
+               arrows: false
+            }
+         }
+      ]
+   });
+
+   $('.down-slider').slick({
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+   });
+});
